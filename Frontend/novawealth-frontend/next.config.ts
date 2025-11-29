7ea2d2d3281943ca4a51ff3258c84adb7c2cac8c
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2025-11-28 22:29:21 -0300

    next install

---
+++ Frontend/novawealth-frontend/next.config.ts
@@ -0,0 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      // adicione hosts de CDN/Cloudinary em produção
+    ],
+  },
+};
+
+module.exports = nextConfig;
